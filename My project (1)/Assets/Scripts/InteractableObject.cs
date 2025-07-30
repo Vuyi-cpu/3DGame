48f9a7ff3b641ca743f27e0f5165884338a778cd
Vuyi-cpu <vuyim1907@gmail.com>
2025-07-30 16:38:24 +0200

    fixed inventory swap

--- My project (1)/Assets/Scripts/InteractableObject.cs
+++ My project (1)/Assets/Scripts/InteractableObject.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class InteractableObject : MonoBehaviour
 {
+    public GameObject emptyIM;
+    public GameObject swordIM;
     public string ItemName;
     public SelectionManager SelectionManager;
     public GameObject Weapon;
 
     private void Update()
     {
+        swordIM.SetActive(false);
         if (Input.GetKeyDown(KeyCode.E) && SelectionManager.playerCanInteract == true)
         {
             Debug.Log("Item added to inventory");
             Destroy(gameObject);
+            emptyIM.SetActive(false);
+            swordIM.SetActive(true);
             Weapon.SetActive(true);
         }
     }
