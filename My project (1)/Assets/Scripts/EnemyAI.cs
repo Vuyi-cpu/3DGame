066849eb23a42d3252140cb6c94f576b31735a5a
Zwad1220 <khanziyad065@gmail.com>
2025-08-12 11:35:57 +0200

    EnemyAI

---
+++ My project (1)/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,74 @@
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+using static UnityEngine.GraphicsBuffer;
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform player;
+    public LayerMask GroundCheck, PlayerCheck;
+    public GameObject projectile;
+
+    //Patrolling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attacking
+    public float timeDelayAttacks;
+    bool Attacked;
+
+    //States
+    public float sightDistance, attackDistance;
+    public bool playerSeenDistance, playerAttackDistance;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        playerSeenDistance = Physics.CheckSphere(transform.position, sightDistance, PlayerCheck);
+        playerAttackDistance = Physics.CheckSphere(transform.position, attackDistance, PlayerCheck);
+        if (!playerSeenDistance && !playerAttackDistance) Patrolling();
+        if (playerSeenDistance && !playerAttackDistance) agent.SetDestination(player.transform.position); ;
+        if (playerSeenDistance && playerAttackDistance) AttackPlayer();
+    }
+    private void Patrolling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+        else { agent.SetDestination(walkPoint); }
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+        if (distanceToWalkPoint.magnitude < 1f) walkPointSet = false;
+    }
+
+    private void SearchWalkPoint()
+    {
+        float randZ = Random.RandomRange(-walkPointRange, walkPointRange);
+        float randX = Random.RandomRange(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randX, transform.position.y, transform.position.z + randZ);
+        if(Physics.Raycast(walkPoint, -transform.up, 2f, GroundCheck)) walkPointSet = true;
+    }
+
+    private void AttackPlayer()
+    {
+        agent.SetDestination(transform.position);
+        transform.LookAt(player);
+        if(!Attacked)
+        {
+            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+            //rb.AddForce(transform.up * 8f, ForceMode.Impulse);
+            Invoke(nameof(ResetAttack), timeDelayAttacks);
+            Attacked = true;
+        }
+    }
+
+    private void ResetAttack()
+    {
+        Attacked = false;
+    }
+}
