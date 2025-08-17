81e568320b07f6b571241f2ff73f7f3f6726430a
Zwad1220 <khanziyad065@gmail.com>
2025-08-17 20:27:08 +0200

    Finished tutorial

--- My project (1)/Assets/Scripts/ButtonGotIt.cs
+++ My project (1)/Assets/Scripts/ButtonGotIt.cs
@@ -3,8 +3,24 @@ using UnityEngine;
 public class ButtonGotIt : MonoBehaviour
 {
     public GameObject tutorial;
-    public void gotIt()
+    public PlayerMovement PlayerMovement; 
+    public MouseMovement MouseMovement;   
+
+    public void Start()
+    {
+        PlayerMovement.enabled = false;
+        MouseMovement.enabled = false;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void CloseTutorial()
     {
         tutorial.SetActive(false);
+        PlayerMovement.enabled = true;
+        MouseMovement.enabled = true;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 }
