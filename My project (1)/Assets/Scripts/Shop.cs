74c00bddf6a00d8c4473d82235b1a4a7f7e90efc
Vuyi-cpu <vuyim1907@gmail.com>
2025-09-21 21:43:51 +0200

    pulsing text

--- My project (1)/Assets/Scripts/Shop.cs
+++ My project (1)/Assets/Scripts/Shop.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using System.Collections;
 
 public class Shop : MonoBehaviour
 {
@@ -18,14 +19,20 @@ public class Shop : MonoBehaviour
     public Enemystate enemystate;
     public PlayerState playerState;
 
-   
+    public float pulseScale = 1.5f;
+    public float pulseDuration = 0.2f;
+    public TextMeshProUGUI insufficientFundsText;
+    private bool isPulsing = false;
+    private Vector3 originalScale;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Awake()
     {
-        
+        if (insufficientFundsText != null)
+        {
+            originalScale = insufficientFundsText.transform.localScale;
+        }
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -34,27 +41,30 @@ public class Shop : MonoBehaviour
 
     public void scytheUpgrade ()
     {
-        if (neuronCount >= 0)
+        if (neuronCount >= 5)
         {
+            insufficientFundsText.gameObject.SetActive(false);
             throwWeapon.scytheDamage += 5;
             upgradeScytheText.text = "Damage: " + throwWeapon.scytheDamage.ToString() + " -> " + (throwWeapon.scytheDamage + 5f).ToString();
         }
         else
         {
-            
+            StartCoroutine(PulseText(insufficientFundsText));
+           
         }
     }
 
     public void katanaUpgrade()
     {
-        if (neuronCount >= 0)
+        if (neuronCount >= 5)
         {
+            insufficientFundsText.gameObject.SetActive(false);
             katanaDamage += 5;
             upgradeKatanaText.text = "Damage: " + katanaDamage.ToString() + " -> " + (katanaDamage + 5f).ToString();
         }
         else
         {
-
+            StartCoroutine(PulseText(insufficientFundsText));
         }
     }
 
@@ -62,13 +72,52 @@ public class Shop : MonoBehaviour
     {
         if (neuronCount >= 0)
         {
+            insufficientFundsText.gameObject.SetActive(false);
             playerState.maxHealth += 100;
             playerState.currentHealth += 100;
             upgradeHealthText.text = "Health: " + playerState.maxHealth.ToString() + " -> " + (playerState.currentHealth + 100f).ToString();
         }
         else
         {
-
+            StartCoroutine(PulseText(insufficientFundsText));
         }
     }
+
+    private IEnumerator PulseText(TextMeshProUGUI text)
+    {
+        if (isPulsing)
+        {
+            text.transform.localScale = originalScale; // reset if mid-pulse
+            isPulsing = false;
+        }
+
+        isPulsing = true;
+        text.gameObject.SetActive(true);
+
+        Vector3 targetScale = originalScale * pulseScale;
+        float timer = 0f;
+
+        // Scale up
+        while (timer < pulseDuration)
+        {
+            text.transform.localScale = Vector3.Lerp(originalScale, targetScale, timer / pulseDuration);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        text.transform.localScale = targetScale;
+
+        // Scale back down
+        timer = 0f;
+        while (timer < pulseDuration)
+        {
+            text.transform.localScale = Vector3.Lerp(targetScale, originalScale, timer / pulseDuration);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        text.transform.localScale = originalScale;
+
+        isPulsing = false;
+    }
+
+
 }
