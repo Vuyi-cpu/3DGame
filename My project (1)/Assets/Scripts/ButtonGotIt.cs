ab0a71b28d05baac4e6be79917149153d2b53ebe
Vuyi-cpu <vuyim1907@gmail.com>
2025-09-22 22:23:18 +0200

    screens

--- My project (1)/Assets/Scripts/ButtonGotIt.cs
+++ My project (1)/Assets/Scripts/ButtonGotIt.cs
@@ -13,11 +13,13 @@ public class ButtonGotIt : MonoBehaviour
     public bool katanaActive;
     public bool scytheActive;
     public bool shopActive;
-
+    PlayerState playerlife;
+    public GameObject deathScreen;
     public void Start()
     {
         PlayerMovement.enabled = false;
         MouseMovement.enabled = false;
+        playerlife = FindFirstObjectByType<PlayerState>();
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
         EventSystem.current.SetSelectedGameObject(buttonfirst);
@@ -28,6 +30,7 @@ public class ButtonGotIt : MonoBehaviour
         if (katanaActive) Destroy(katanaTutorial);
         if (scytheActive) Destroy(scytheTutorial);
         if (shopActive) Destroy(shopTutorial);
+        if (playerlife.currentHealth<=0) deathScreen.SetActive(true);
         Time.timeScale = 1f;
         tutorial.SetActive(false);
         PlayerMovement.enabled = true;
