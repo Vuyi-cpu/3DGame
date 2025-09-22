82e8abc288a22f8617dec38c8d3738a71861919b
Vuyi-cpu <vuyim1907@gmail.com>
2025-09-22 23:38:59 +0200

    doors

--- My project (1)/Assets/Scripts/BulletHit.cs
+++ My project (1)/Assets/Scripts/BulletHit.cs
@@ -4,6 +4,6 @@ public class BulletHit : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player") || other.CompareTag("Environment")) Destroy(gameObject);
+        if (other.CompareTag("Player") || other.CompareTag("Environment") || other.CompareTag("door")) Destroy(gameObject);
     }
 }
