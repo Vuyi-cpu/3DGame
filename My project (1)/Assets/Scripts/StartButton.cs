040dcaa44e6711415cbaac457a245ae026ae0526
KNA-0E <koena2mathibeli@gmail.com>
2025-10-18 18:37:40 +0200

    fade in

--- My project (1)/Assets/Scripts/StartButton.cs
+++ My project (1)/Assets/Scripts/StartButton.cs
@@ -1,18 +1,31 @@
 using UnityEngine;
+using System.Collections;
 
 public class StartButton : MonoBehaviour
 {
-    public GameObject startButton;
+    public CanvasGroup startButtonGroup;
     public float delay = 5f;
+    public float fadeDuration = 1.5f;
 
     void Start()
     {
-        startButton.SetActive(false);
-        Invoke(nameof(ShowButton), delay);
+        startButtonGroup.alpha = 0f;
+        startButtonGroup.gameObject.SetActive(true);
+        StartCoroutine(FadeInButton());
     }
 
-    void ShowButton()
+    IEnumerator FadeInButton()
     {
-        startButton.SetActive(true);
+        yield return new WaitForSeconds(delay);
+
+        float elapsed = 0f;
+        while (elapsed < fadeDuration)
+        {
+            elapsed += Time.deltaTime;
+            startButtonGroup.alpha = Mathf.Clamp01(elapsed / fadeDuration);
+            yield return null;
+        }
+
+        startButtonGroup.alpha = 1f;
     }
 }
